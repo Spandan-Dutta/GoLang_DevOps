dd9c2d32580c8c9bbc48f2e5df7b55ce8494e038
Spandan Dutta <spandydutta@gmail.com>
2024-06-13 01:39:24 +0530

    new commit

---
+++ hello_world.go
@@ -0,0 +1,11 @@
+package main
+// In Go, every file belongs to a package, and main is a special package name. A package named main indicates that this file is an executable program (as opposed to a library).
+
+import "fmt"
+// The import statement is used to include the fmt package, which contains functions for formatted I/O (input/output) operations. In this case, it is used to access the Println function.
+
+func main() {
+	fmt.Println("Hello World everyone")
+}
+
+
